5946ae4a61cc48a8bea52de9b44daf2a617dc3be
NeilB18 <82923875+NeilB18@users.noreply.github.com>
2023-06-25 18:40:02 -0700

    added files

---
+++ Project 1/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split 
+df =pd.read_excel('data.xlsx')
+x = df.iloc[:, :-1].values # ':' represents all the rows and ':-1' represents all the columns except the last one
+y = df.iloc[:,-1].values
+from sklearn.model_selection import train_test_split 
+# we make our vairables for training and testing the model.
+# test_size = 0.2 means that we are goin to be using 20% of the dataset as our testing data.
+# random_state = 0 is a way of making sure that we have the sam test set and trainging set everytime the model is run,
+# because the function train_test_split randomly selects data therefore it is important for us to make sure we are using
+# the same training and testing set
+x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=0.2, random_state=0)

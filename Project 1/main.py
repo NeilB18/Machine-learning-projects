d35e358f2b5ffe5ad9f8d0153106fdfc187732b1
NeilB18 <82923875+NeilB18@users.noreply.github.com>
2023-06-25 18:48:54 -0700

    ...

--- Project 1/main.py
+++ Project 1/main.py
@@ -12,8 +12,6 @@ from sklearn.model_selection import train_test_split
 # because the function train_test_split randomly selects data therefore it is important for us to make sure we are using
 # the same training and testing set
 x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=0.2, random_state=0)
-
-
 model = LinearRegression()
 model.fit(x_train,y_train)
 y_pred = model.predict(x_test)
